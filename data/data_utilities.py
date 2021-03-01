5805fd83a94eda40e99dafbecf51c76bdd594715
Rodrigo <rvarasmi@tepper.cmu.edu>
2021-03-01 01:01:34 -0500

    options monitor

--- data/data_utilities.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import logging as log
-
-from typing import Any, Dict, List, Tuple
-
-
-def foo():
-    return True
--- data/yahoo_data.py
+++ data/yahoo_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import logging as log
 
-from datetime import datetime as dt
+import datetime as dt
 from yahoo_fin import options as yo
 
 import core.constants as c
@@ -11,12 +11,12 @@ log.basicConfig(format='%(levelname)s:%(message)s', level=log.INFO)
 
 def get_all_calls(ticker: str) -> pd.DataFrame:
     dates = yo.get_expiration_dates(ticker)
-    dates = [dt.strptime(da, c.YAHOO_DATE_FMT) for da in dates]
+    dates = [dt.datetime.strptime(da, c.YAHOO_DATE_FMT) for da in dates]
     log.info(f'Calls found for {ticker} with dates: {dates}')
     calls_df = pd.concat(
         {da: yo.get_calls(ticker=ticker, date=da).set_index('Contract Name') for da in dates},
         0,
         names=['Date', 'Contract Name']
     )
-
+    calls_df['Implied Volatility'] = calls_df['Implied Volatility'].str.replace('%', '').astype(float).div(100)
     return calls_df
