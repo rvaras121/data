91d4a714ca2463f0c46056195843bb1e32f40e46
Rodrigo <rvarasmi@tepper.cmu.edu>
2021-02-28 22:59:20 -0500

    Constants1

---
+++ data/yahoo_data.py
@@ -0,0 +1,26 @@
+import fin as fin
+import numpy as np
+import pandas as pd
+import logging as log
+
+from datetime import datetime as dt
+from yahoo_fin import options as yo
+from typing import Any, Dict, List, Tuple
+
+import data.data.data_utilities as d
+import core.constants as c
+
+log.basicConfig(format='%(levelname)s:%(message)s', level=log.INFO)
+
+
+def get_all_calls(ticker: str) -> pd.DataFrame:
+    dates = yo.get_expiration_dates(ticker)
+    dates = [dt.strptime(da, c.YAHOO_DATE_FMT) for da in dates]
+    log.info(f'Calls found for {ticker} with dates: {dates}')
+    calls_df = pd.concat(
+        {da: yo.get_calls(ticker=ticker, date=da).set_index('Contract Name') for da in dates},
+        0,
+        names=['Date', 'Contract Name']
+    )
+
+    return calls_df
